43a91414e60640cf1dee5a5cf060b5dc59662f95
ZavierPan <>
2021-06-29 19:49:12 +0800

    Add models scripts

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -1,5 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://zavier:zxcS840831@cluster0.vdkoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
